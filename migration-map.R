580987c0467d39c84a16a6c0e947ecfc43084688
Brandon Edwards <brandon.edwards144@gmail.com>
2017-05-10 10:01:54 -0400

    Added R script for migration map

---
+++ migration-map.R
@@ -0,0 +1,17 @@
+##############################
+#
+# Migration Map
+#
+# Visualize migrations of
+# different bird species
+# from eBird data.
+#
+# Brandon Edwards
+# May 2017
+##############################
+
+##############################
+# Clear Memory
+##############################
+
+remove(list = ls())
