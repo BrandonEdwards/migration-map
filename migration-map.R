a7a4f2f17c5ad61b97ee1f8d3ca67811ebd2a889
Brandon Edwards <brandon.edwards144@gmail.com>
2017-05-10 13:04:02 -0400

    Functional script with output of single points/observation/day

--- migration-map.R
+++ migration-map.R
@@ -26,7 +26,7 @@ library(rworldmap)
 ##############################
 # File IO
 ##############################
-pipl <- read.csv(file.choose(), header = TRUE, sep = "\t")
+pipl <- read.csv("data.txt", header = TRUE, sep = "\t")
 
 ##############################
 # Create map
@@ -34,5 +34,21 @@ pipl <- read.csv(file.choose(), header = TRUE, sep = "\t")
 
 #coordinates obtained from http://www.latlong.net/
 coords <- read.csv("coords.csv")
+
+#creates empty map object
 map <- getMap(resolution = "low")
-plot(map, xlim = range(coords$lon), ylim = range(coords$lat), asp = 1)
+#get list of all unique dates and sort them from oldest to newest
+pipl$dates <- substr(pipl$OBSERVATION.DATE, start = 6, stop = 10)
+dates <- sort(unique(pipl$dates))
+
+for (i in 2:length(dates))
+{
+  toPlot <- pipl[ which(pipl$dates == dates[i]), ]
+  plot(map, xlim = range(coords$lon), ylim = range(coords$lat), asp = 1)
+  points(toPlot$LONGITUDE, toPlot$LATITUDE, col = "blue")
+  p <- recordPlot()
+  png(paste("C:/Users/Brandon/Documents/GitHub/migration-map/img/", i, ".png", sep=""), width=1000, height=872)
+  print(p)
+  dev.off()
+  plot.new()
+}
