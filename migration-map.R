29c339ce1a3dc0e7cc134a29d2b635cdcaa78ce7
Brandon Edwards <brandon.edwards144@gmail.com>
2017-05-10 10:34:18 -0400

    Read pipl data, coordinate data, and generate map

--- migration-map.R
+++ migration-map.R
@@ -15,3 +15,24 @@
 ##############################
 
 remove(list = ls())
+
+##############################
+# Libraries to Use
+##############################
+
+library(rworldmap)
+#library(ggmap)
+
+##############################
+# File IO
+##############################
+pipl <- read.csv(file.choose(), header = TRUE, sep = "\t")
+
+##############################
+# Create map
+##############################
+
+#coordinates obtained from http://www.latlong.net/
+coords <- read.csv("coords.csv")
+map <- getMap(resolution = "low")
+plot(map, xlim = range(coords$lon), ylim = range(coords$lat), asp = 1)
