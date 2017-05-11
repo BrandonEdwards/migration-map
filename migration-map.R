34e346ff0359ec47837c25ced306a09bf04318d8
Brandon Edwards <brandon.edwards144@gmail.com>
2017-05-11 09:54:17 -0400

    Fixed col references to go with fread()

--- migration-map.R
+++ migration-map.R
@@ -34,21 +34,18 @@ pipl <- fread("data.txt")
 # Create map
 ##############################
 
-#coordinates obtained from http://www.latlong.net/
-coords <- read.csv("coords.csv")
-
 #creates empty map object
 map <- get_map(location = 'Iowa', zoom = 4)
 
 #get list of all unique dates and sort them from oldest to newest
-pipl$dates <- substr(pipl$OBSERVATION.DATE, start = 6, stop = 10)
+pipl$dates <- substr(pipl$`OBSERVATION DATE`, start = 6, stop = 10)
 dates <- sort(unique(pipl$dates))
 
 for (i in 2:length(dates))
 {
   toPlot <- pipl[ which(pipl$dates == dates[i]), ]
   p <- ggmap(map, legend.position = "none", main = dates[i])
-  toPlot$count <- as.numeric(toPlot$OBSERVATION.COUNT)
+  toPlot$count <- as.numeric(toPlot$`OBSERVATION COUNT`)
   toPlot$count = (toPlot$count)/2
   p <- p + geom_point(data = toPlot, aes(x = LONGITUDE, y = LATITUDE, colour = "red", size = count), show.legend = FALSE)
   p <- p + ggtitle(dates[i])
